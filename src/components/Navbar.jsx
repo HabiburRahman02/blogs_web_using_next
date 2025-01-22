fdaa7418ca7584bc05df8283960f5d646b311dd0
Habibur Rahman <habibur88454@gmail.com>
2025-01-23 00:43:40 +0600

    added login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
 "use client"
+import { useKindeAuth } from '@kinde-oss/kinde-auth-nextjs';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const { getUser, isAuthenticated } = useKindeAuth();
+    const [user, setUser] = useState(null);
+    console.log(user);
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            const userData = getUser();
+            setUser(userData);
+        } else {
+            setUser(null);
+        }
+    }, [isAuthenticated]);
+
     return (
         <div className="bg-gray-800 text-white p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-xl font-bold">My Blogs</div>
+                <Link href='/'>
+                    <div className="text-xl font-bold">My Blogs</div>
+                </Link>
                 <nav>
                     <ul className='flex space-x-4'>
                         <li>
@@ -14,16 +30,19 @@ const Navbar = () => {
                                 Home
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="hover:underline">
-                                Profile
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/" className="hover:underline">
-                                Login
-                            </Link>
-                        </li>
+                        {user ? (
+                            <li>
+                                <Link href="/profile" className="hover:underline">
+                                    Profile
+                                </Link>
+                            </li>
+                        ) : (
+                            <li>
+                                <Link href="/login" className="hover:underline">
+                                    Login
+                                </Link>
+                            </li>
+                        )}
                     </ul>
                 </nav>
             </div>
